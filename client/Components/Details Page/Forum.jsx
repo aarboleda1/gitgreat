bcba071a35787f135338c933df48e424cac2f810
Nicholas Wang <nickwang55@gmail.com>
2016-12-14 09:43:35 -0800

    Comment for some logic behind forum

--- client/Components/Details Page/Forum.jsx
+++ client/Components/Details Page/Forum.jsx
@@ -4,15 +4,44 @@ class Forum extends React.component {
     super(props);
 
     this.state = {
-
+      value: '',
+      messages: []
     };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  // AJAX get request to the database to fetch the messages and update state with it
+  // Probably should be in component did mount
+  fetchMessages() {
+
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    alert('A name was submitted: ' + this.state.value);
+    event.preventDefault();
   }
 
   render() {
     return (
       <div className="forum-box">
-        <div className="new-message-box">
+
+        <div className="send-box">
+          <form onSubmit={this.handleSubmit}>
+            <input type="text" value={this.state.value} onChange={this.handleChange} />
+            <input type="submit" value="Send" />
+          </form>
         </div>
+
+        <div className="message-box">
+
+        </div>
+
       </div>
     );
   }
