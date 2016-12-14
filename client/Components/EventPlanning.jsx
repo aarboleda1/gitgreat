ae3cd60a56930438b8a3316cabde129f7330dc82
Anton <anton.arboleda2gmail.com>
2016-12-13 22:16:32 -0800

    Add click event to hide sidebar

--- client/Components/EventPlanning.jsx
+++ client/Components/EventPlanning.jsx
@@ -10,6 +10,8 @@ class EventPlanning extends React.Component {
     };
 
     this.changeDisplay = this.changeDisplay.bind(this);
+    this.openNav = this.openNav.bind(this);
+    this.closeNav = this.closeNav.bind(this);
   }
   
   changeDisplay(e) {
@@ -17,6 +19,15 @@ class EventPlanning extends React.Component {
     this.setState({
       tab: e.target.className
     });
+  }
+
+  openNav() {
+    document.getElementById("slide-out").style.width = "250px";
+  }
+
+  closeNav() {
+    console.log('CLICKED')
+    document.getElementById("slide-out").style.width = "0";
   }
 
 
@@ -34,7 +45,7 @@ class EventPlanning extends React.Component {
     return (
       <div>      
         {/*<h2 className="eventHeader">{this.props.featuredEvent.name} | {this.props.featuredEvent.where} | {this.props.featuredEvent.when}</h2>*/}
-        <FeatureNavigation changeDisplay={this.changeDisplay} />
+        <FeatureNavigation openNav={this.openNav} closeNav={this.closeNav} changeDisplay={this.changeDisplay} />
         {view}
       </div>     
     ) 
--- client/Components/FeatureNavigation.jsx
+++ client/Components/FeatureNavigation.jsx
@@ -3,8 +3,8 @@
 
 var FeatureNavigation = (props) => (
   // var sidebarClass = this.props.isOpen ? 'sidebar open' : 'sidebar';
-  <div className="nav-animate">
-  <ul id="slide-out" className="side-nav">
+  <div className="mySidenav">
+  <ul id="slide-out" className="side-nav" onClick={() => props.closeNav()}>
     <li><a href='#' className="wtbBtn" id="firstBtn" value="whatToBringBtn" onClick={(e) => props.changeDisplay(e)}>What To Bring</a></li>
     <li><a href='#' className="reminderBtn" value="reminderBtn" onClick={(e) => props.changeDisplay(e)}>Reminders</a></li>
     <li><a href='#' className="photosBtn" value="photosBtn" onClick={(e) => props.changeDisplay(e)}>Photos</a></li>
