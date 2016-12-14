6f373efe24b7d54b41acecbd77652d077d2531c6
Nicholas Wang <nickwang55@gmail.com>
2016-12-13 20:19:05 -0800

    Initial setup of Detail and Descript components

---
+++ client/Components/Details Page/Description.jsx
@@ -0,0 +1,10 @@
+var Description = (props) => (
+  <div>
+    <h1>props.featuredEvent</h1>
+    <div className="description-box">
+      props.eventDescription
+    </div>
+  </div>
+);
+
+window.Description = Description;
---
+++ client/Components/Details Page/Details.jsx
@@ -0,0 +1,17 @@
+class Details extends React.component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isPlanner: false,
+      username: 'Test',
+      featuredEvent: this.props.featuredEvent,
+      eventDescription: '',
+      location: '',
+      address: ''
+    };
+  }
+
+  render() {
+
+  }
+}
