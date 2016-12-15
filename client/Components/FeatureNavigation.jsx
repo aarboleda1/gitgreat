2ba5f0fc0d333543163e358220066f1888904738
Anton <anton.arboleda2gmail.com>
2016-12-14 17:40:55 -0800

    Significant progress on webpack/react-router

--- client/Components/FeatureNavigation.jsx
+++ client/Components/FeatureNavigation.jsx
@@ -5,18 +5,15 @@ import { Link } from 'react-router'
 
 var FeatureNavigation = (props) => (
   <div className="mySidenav">
-  <ul id="slide-out" className="side-nav" onClick={() => props.closeNav()}>
-    <li><Link to="/wtb">What To Bring</Link></li>
-    {/*<li><a href='#' className="wtbBtn" id="firstBtn" value="whatToBringBtn" onClick={(e) => props.changeDisplay(e)}>What To Bring</a></li>*/}
-    <li><a href='#' className="reminderBtn" value="reminderBtn" onClick={(e) => props.changeDisplay(e)}>Reminders</a></li>
-    <li><a href='#' className="photosBtn" value="photosBtn" onClick={(e) => props.changeDisplay(e)}>Photos</a></li>
-    <li><a href='#' className="chatBtn" value="chatBtn" onClick={(e) => props.changeDisplay(e)}>Chatroom (IP)</a></li>
-    <li><a>Close Navigation</a></li>    
-  </ul>
+    <ul id="slide-out" className="side-nav" onClick={() => props.closeNav()}>
+      <li><Link to="/wtb">What To Bring</Link></li>
+      <li><Link to="/reminders">Reminders</Link></li>
+      <li><Link to="/photos">Photos</Link></li>
+      <li><a>Close Navigation</a></li>    
+    </ul>
     <a href="#" data-activates="slide-out" className="button-collapse"><i onClick={() => props.openNav()} className="material-icons">Navigation</i></a>
   </div>
 );
 
 export default FeatureNavigation;
 
-window.FeatureNavigation = FeatureNavigation;
--- client/Main.js
+++ client/Main.js
@@ -4,16 +4,20 @@ import ReactDOM from 'react-dom';
 import HomepageApp from './Components/HomepageApp.jsx';
 import CreateEventApp from './Components/CreateEventApp.jsx';
 import WhatToBring from './Components/WhatToBring.jsx';
+import Reminders from './Components/Reminders.jsx';
+import Photos from './Components/Photos.jsx';
 import { Router, Route, hashHistory } from 'react-router';
 
 
-//render react router
+
 ReactDOM.render((
   <Router history={hashHistory}>
     <Route path="/" component={HomepageApp}/>
-    <Route path="/create" component={CreateEventApp}/>
-    <Route path="/wtb" component={WhatToBring}/>
+      <Route path="/create" component={CreateEventApp}/>
+      <Route path="/wtb" component={WhatToBring}/>
+      <Route path="/reminders" component={Reminders}/>
+      <Route path="/photos" component={Photos}/>      
   </Router>
-),document.getElementById('app'));
+),document.getElementById('app'))
 
 
